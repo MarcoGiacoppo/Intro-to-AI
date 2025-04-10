67641595d84dc0dd08d69a251c552468e70b0426
Jason Tjahjono <jason.tjahjono2503@gmail.com>
2025-04-10 10:54:58 +0700

    made bat file output debug lines, changed some syntax

--- generator.py
+++ generator.py
@@ -2,12 +2,12 @@ import random
 
 def generate_test_case(
     filename,
-    num_nodes=3,
+    num_nodes=8,
     coord_range=(0, 10),
-    max_edges_per_node=3,
+    max_edges_per_node=2,
     cost_range=(1, 10),
     bidirectional_chance=0.5,
-    num_destinations=2
+    num_destinations=1
 ):
     nodes = {}
     edges = {}
